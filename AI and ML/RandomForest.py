6a8a1003ba3ae52ebce9a024da80a590919cb500
Otwiine Olweny <otwiine@gmail.com>
2025-04-24 11:30:16 +0300

    Update RandomForest.py

--- AI and ML/RandomForest.py
+++ AI and ML/RandomForest.py
@@ -20,7 +20,7 @@ X = flat_data[valid_pixels]
 Y = labels[valid_pixels]
 
 # Split into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y_test_size=0.3, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.3, random_state=42)
 
 # Train Random Forest model
 rf_model = RandomForestClassifier(
