903b63140a0e6bfdb950f1cbd82c87337a0f1fa7
Otwiine Olweny <otwiine@gmail.com>
2025-10-03 12:23:33 +0300

    Your First Python Program

---
+++ Python/First_Program.py
@@ -0,0 +1,11 @@
+# This is a comment - Python ignores everything after the # symbol
+print("Hello, World!")
+print("Welcome to Python programming!")
+
+# You can print multiple things at once
+print("My name is:", "Otwiine Olweny")
+
+# Print with different separators
+print("Python", "is", "awesome", sep=" - ")
+print("Python", "is", "epic", sep=" * ")
+print("Python", "is", "fun", sep=" $ ")
