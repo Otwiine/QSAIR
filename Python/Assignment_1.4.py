2dadb2a3f642e2c8c04c18cbeb3ee19f7a2fc925
Otwiine Olweny <otwiine@gmail.com>
2025-10-04 14:44:15 +0300

    Circle Area Converter

---
+++ Python/Assignment_1.4.py
@@ -0,0 +1,10 @@
+# A=πr2
+# C=2πr
+# π = 3.14159
+
+radius = float(input("What is the radius of your Circle ?: "))
+area = 3.14159 * (radius ** 2)
+circum = 2 * 3.14159 * radius
+
+print(f"The Area of your Circle is: {area}") # units ^2
+print(f"The Circumference of your Circle is {circum}")
