a9865513e391f3338f91a606be4512d12da7f7ee
Otwiine Olweny <otwiine@gmail.com>
2025-10-04 14:42:54 +0300

    Personal Information Display

---
+++ Python/Assignment_1.1.py
@@ -0,0 +1,5 @@
+name = input("What is your name: ")
+age = int(input("How old are you: "))
+fav_color = input("What is your favorite color: ")
+
+print(f"Hello {name}, you are {age} years old, and like the color {fav_color}!")
